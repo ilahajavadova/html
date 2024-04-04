c7d4c0f3575a503893c9fe15bf93b8460c323e16
ilahajavadova <163039213+ilahajavadova@users.noreply.github.com>
2024-04-04 19:24:09 +0400

    Add files via upload

---
+++ 04.04/app.js
@@ -0,0 +1,84 @@
+"use strict";
+// let arr = [14, 20, 35, 40, 57, 60, 100];
+// let sum = 0
+
+// let Sum = (arr) => {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 20 == 0) {
+//             sum += arr[i];
+//         }
+//     }
+//     return sum;
+// };
+// console.log(Sum(arr));
+
+
+
+
+// let count = 0;
+
+// const charCount = (sent, char) => {
+//     for (let i = 0; i <= sent.lenght; i++) {
+//         if (sent[i] == char) {
+//             count++
+//         }
+//     }
+//     return count;
+// }
+
+// let sentences = prompt("Cumleni daxil edin");
+// let char = prompt("Herfi daxil edin");
+
+// console.log(charCount(sentences, char));
+
+
+
+
+// let arr = [23, 341, 4000, 9, 99, 817, 0]
+// let checkDigit = (numbers ) => {
+
+//     for (let i = 0; i <= numbers.length; i++) {
+//         let number = numbers[i];
+//         let count = 0
+
+
+//     }
+
+
+// }
+
+
+
+//  let count = 0;
+
+//  const charCount = ( sent, word) => {
+//  for (let i = 0; i < word.length; i++) {
+//     if (sent[i] == word){
+//         count++
+//     }
+// }
+//  return count;
+    
+//  } 
+ 
+// let sentences = prompt(" Cumleni daxil edin");
+// let word = prompt("");
+// console.log(charCount(sentences,word));
+
+
+
+// const fruits = [ "Banana", "Orange", "Apple", "Mango"]
+
+// let result = fruits.length
+// let result = fruits.toString
+// let result= fruits.at
+// let result=fruits.join("  ")
+// let result= fruits.pop
+// let result = fruits.shift("melon ")
+// let result = fruits.unshift( "melon") 
+// let result = fruits.delete ( "Banana" )
+// let result = fruits.copyWithin(1,2)
+// let result = fruits.splice ( 3 , 0, "Armud")
+// let result = fruits.toSpliced ( 1 , 0, "")
+// let result = fruits.slice ( )
+// console.log(fruits)
