021e1786cfe3cde9ce096af26d4ec1a35fd96787
ilahajavadova <163039213+ilahajavadova@users.noreply.github.com>
2024-04-08 16:24:35 +0400

    Add files via upload

---
+++ 3.13/app.js
@@ -0,0 +1,33 @@
+"use strict"
+
+// const date = new Date();
+// const hour = date.getHours();
+// const min = date.getMinutes();
+// console.log(hour)
+
+
+
+// let hour = Math.floor( Math.random() * 24);
+// console.log(hour);
+
+// if (hour<12 && hour >6){
+// console.log("Sabahiniz xeyir");
+
+// }else if (hour > 12 && hour<18){
+//     console.log("Gunortaniz xeyir");
+// } else{
+//     console.log("Axshaminiz xeyir")
+
+// }
+
+
+// let checkOddEvenNumber = (...numbers) => {
+
+//     let oddNumbers = numbers.filter((number) => number % 2 === 0);
+
+//     let evenNumbers = numbers.filter((number) => number % 2 !== 0);
+//     console.log('Tek ededler  $ {oddNumbers}')
+//     console.log('Cut ededler $ {evenNumbers}')
+// }
+// console.log(checkOddEvenNumber(23, 34, 46, 22, 80, 124));
+
